81eebc2f0cc25607bbd1a21d7b3bb719b5478c60
andrewfarm <Andrew.Farm@tufts.edu>
2017-07-31 19:00:17 -0400

    Add error message for missing webgl2

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 var canvas = document.getElementById('canvas');
-//const pixelRatio = window.devicePixelRatio || 1;
-//console.log('window.devicePixelRatio', window.devicePixelRatio);
-canvas.style.width = window.innerWidth;
-canvas.style.height = window.innerHeight;
-canvas.width = window.innerWidth;// * pixelRatio;
-canvas.height = window.innerHeight;// * pixelRatio;
-var rect = canvas.getBoundingClientRect();
 
-const gl = canvas.getContext('webgl') ||
-        canvas.getContext('experimental-webgl');
-const universe = new Universe(gl);
-canvas.addEventListener('mousemove',
-        function(event) {
-                universe.mouseMoved(event.clientX - rect.left, event.clientY - rect.top);
-        });
+const gl = canvas.getContext('webgl2');
+if (gl) {
+        //const pixelRatio = window.devicePixelRatio || 1;
+        //console.log('window.devicePixelRatio', window.devicePixelRatio);
+        canvas.style.width = window.innerWidth;
+        canvas.style.height = window.innerHeight;
+        canvas.width = window.innerWidth;// * pixelRatio;
+        canvas.height = window.innerHeight;// * pixelRatio;
+        var rect = canvas.getBoundingClientRect();
+        
+        const universe = new Universe(gl);
+        canvas.addEventListener('mousemove',
+                function(event) {
+                        universe.mouseMoved(
+                                event.clientX - rect.left,
+                                event.clientY - rect.top);
+                });
 
-function render() {
-        universe.nextFrame();
-        requestAnimationFrame(render);
+        function render() {
+                universe.nextFrame();
+                requestAnimationFrame(render);
+        }
+        render();
+} else {
+        document.body.innerHTML =
+                '<div>\
+                        <h1 class="red">Critical Failure</h1>\
+                        <p>WebGL 2 missing from browser<p>\
+                        <h2 class="green">2 solutions found</h2>\
+                        <ul>\
+                                <li><a href="http://firefox.com">Use Firefox &raquo;</a></li>\
+                                <li><a href="http://chrome.com">Use Chrome &raquo;</a></li>\
+                        </ul>\
+                </div>';
 }
-render();
